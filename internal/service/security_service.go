9cd79817c4cc781718db190b487a7271417be36a
Dan Munro <dan@danmunro.com>
2023-01-14 11:20:35 -0800

    error checking

--- internal/service/security_service.go
+++ internal/service/security_service.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"errors"
 	"github.com/google/uuid"
 	"github.com/third-place/user-service/internal/db"
 	"github.com/third-place/user-service/internal/entity"
@@ -20,29 +21,32 @@ func CreateSecurityService() *SecurityService {
 }
 
 func (s *SecurityService) IsInGoodStanding(session *model.Session) bool {
-	user := s.getUser(session)
-	if user == nil {
+	user, err := s.getUser(session)
+	if err != nil {
 		return false
 	}
 	return !user.IsBanned
 }
 
 func (s *SecurityService) IsModerator(session *model.Session) bool {
-	user := s.getUser(session)
-	if user == nil {
+	user, err := s.getUser(session)
+	if err != nil {
 		return false
 	}
 	return !user.IsBanned && (user.Role == string(model.MODERATOR) || user.Role == string(model.ADMIN))
 }
 
-func (s *SecurityService) getUser(session *model.Session) *entity.User {
+func (s *SecurityService) getUser(session *model.Session) (*entity.User, error) {
+	if session.User == nil {
+		return nil, errors.New("user not defined")
+	}
 	userUuid, err := uuid.Parse(session.User.Uuid)
 	if err != nil {
-		return nil
+		return nil, err
 	}
 	user, err := s.userRepository.GetUserFromUuid(userUuid)
 	if err != nil {
-		return nil
+		return nil, err
 	}
-	return user
+	return user, nil
 }
