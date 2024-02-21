27ddaf75a510c9fcf7fdc49d9aeaf48dfa4250a3
Dan Munro <dan@danmunro.com>
2024-02-20 20:16:36 -0800

    don't check for .env when republishing

--- cmd/republish_user_to_kafka/main.go
+++ cmd/republish_user_to_kafka/main.go
@@ -14,10 +14,7 @@ import (
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	_ = godotenv.Load()
 	username := os.Args[1]
 	kafkaWriter, err := kafka2.CreateProducer()
 	if err != nil {
